7116fe9f51e05e04392696c6d61c61596c99a7d7
ak17990 <48554196+ak17990@users.noreply.github.com>
2025-02-07 16:03:47 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<your subscription id here>"
+  subscription_id = "7da89492-e9b8-42a6-984b-c3607ded124c"
 }
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "jenkinsstatealan"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
